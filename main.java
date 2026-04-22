860b75107ed80bb415012ac868b02e4b79fb0e43
Tanishka <Tanishkacs4061@gmail.com>
2026-04-22 11:39:04 -0400

    Committing the changes related to CashbackApp

--- main.java
+++ main.java
@@ -2,9 +2,6 @@ package CashBackReturn;
 
 public class main {
     public static void main(String[] args) {
-     //   CashBackReturn cashBackReturn = new CashBackReturn();
-        double amount = 100.0; // Example amount
-      //  double cashBack = cashBackReturn.calculateCashBack(amount);
         System.out.println("Cash Back for amount $" + amount + " is: $");
     }
 }
